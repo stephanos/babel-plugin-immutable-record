da2726c025c85c2636593862b1d7d3af89a4dc3e
stephanos <github@stephanbehnke.de>
2016-04-30 14:23:43 +0200

    add more type tests

--- src/types.spec.js
+++ src/types.spec.js
@@ -1,28 +1,40 @@
 /* @flow */
 /* eslint no-unused-vars:0 */
 
+import { List } from 'immutable';
 import ShallowRecord from './fixtures/shallow-record/output';
+import NestedRecord from './fixtures/nested-record/output';
 
-export function createWithAllFields(): ShallowRecord {
+export function createShallowRecordWithAllFields(): ShallowRecord {
   return new ShallowRecord({
+    arrayField: ['1'],
     booleanField: true,
     numberField: 101,
     stringField: 'string',
   });
 }
 
-export function createWithFieldDefault(): ShallowRecord {
+export function createShallowRecordWithDefaults(): ShallowRecord {
   return new ShallowRecord({
     booleanField: true,
     stringField: 'string',
   });
 }
 
-export function update() {
-  const record = createWithFieldDefault();
+export function updateShallowRecord() {
+  const record = createShallowRecordWithAllFields();
   record.update({ stringField: 'new-string' });
 }
 
+export function accessFields() {
+  const record = createShallowRecordWithAllFields();
+
+  const a: List<string> = record.arrayField;
+  const b: bool = record.booleanField;
+  const s: string = record.stringField;
+  const n: number = record.numberField;
+}
+
 export function exportToMap() {
-  createWithFieldDefault.toMap();
+  createShallowRecordWithAllFields().toMap();
 }
